7a515081bfc1f01330973a6f6b5a590a09b514dc
Erik Youngren <artanis.00@gmail.com>
2009-10-05 16:59:00 -0700

    Connect treeview correctly.

--- __main__.py
+++ __main__.py
@@ -14,20 +14,31 @@ store_plot = gtk.TreeStore(str, bool, gtk.gdk.Color, int, int)
 store_plot.append(None, ["sin(x)", True, gtk.gdk.Color(), 1, 1])
 
 # Setup columns
-col_draw = gtk.TreeViewColumn("Plot")
-
-
+# COLUMN str -- Function
 col_function = gtk.TreeViewColumn("Function")
+col_function_cell = gtk.CellRendererText()
+col_function.pack_start(col_function_cell)
+col_function.add_attribute(col_function_cell, "text", 0)
 
+# COLUMN bool -- Plot the function?
+col_draw = gtk.TreeViewColumn("Plot")
+col_draw_cell = gtk.CellRendererToggle()
+col_draw.pack_start(col_draw_cell)
+col_draw.add_attribute(col_draw_cell, "activatable", 1)
 
+# COLUMN gtk.gdk.Color -- Line color
 # TODO: line color
+
+# COLUMN int -- Line width
 # TODO: line width
+
+# COLUMN int -- Line style
 # TODO: line style
 
 tree_plot = ui_tree.get_widget("tree_plot")
+tree_plot.set_model(store_plot)
 tree_plot.append_column(col_draw)
 tree_plot.append_column(col_function)
-tree_plot.set_model(store_plot)
 
 
 window_main = ui_tree.get_widget("window_main")
