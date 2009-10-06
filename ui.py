18015f12a23a06d75476e0e0dee4ebb1e6306412
Erik Youngren <artanis.00@gmail.com>
2009-10-06 01:40:51 -0700

    Some UI changes. I don't remember what...

--- ui.py
+++ ui.py
@@ -1,45 +1,55 @@
 import gtk
 import gtk.glade
 
-__all__ = ["window_main", "store_plot", "tree_plot", "draw_graph"]
+__all__ = ["ui_tree", "window_main", "store_plot", "tree_plot",
+    "draw_graph"]
 
-def onToggle(toggle, path, user_data):
-	model, column = user_data
-	model[path][column] = not model[path][column]
-	return
+def police_graphs(model, path, row_iter):
+    row = model[path]
+    if row.next is not None:
+        if row[0] == "":
+            model.remove(row_iter)
+    elif row[0] != "":
+        model.append(None, ["", False, gtk.gdk.Color(), 1, 1])
 
-def onEdited(cell, path, text, user_data):
-	model, column = user_data
-	model[path][column] = text
-	return
+def onToggle(toggle, path, model):
+	model[path][1] = not model[path][1]
+
+def onEdited(cell, path, text, model):
+	model[path][0] = text
+	model[path][1] = True
+	
+	# Auto create/delete functions as needed
+	model.foreach(police_graphs)
 
 glade_ui = "ui.glade"
 ui_tree = gtk.glade.XML(glade_ui)
 
 signals = {
-    "gtk_main_quit": gtk.main_quit
+    "gtk_main_quit": gtk.main_quit,
 }
+
 ui_tree.signal_autoconnect(signals)
 
 # function, draw, line color, line width, line type
 store_plot = gtk.TreeStore(str, bool, gtk.gdk.Color, int, int)
 store_plot.append(None, ["sin(x)", True, gtk.gdk.Color(), 1, 1])
-store_plot.append(None, ["cos(y)", True, gtk.gdk.Color(), 1, 1])
+store_plot.append(None, ["", False, gtk.gdk.Color(), 1, 1])
 
 # Setup columns
 # COLUMN str -- Function
 col_function = gtk.TreeViewColumn("Function")
 col_function_cell = gtk.CellRendererText()
 col_function_cell.set_property("editable", True)
-col_function_cell.connect("edited", onEdited, (store_plot, 0))
+col_function_cell.connect("edited", onEdited, store_plot)
 col_function.pack_start(col_function_cell)
 col_function.add_attribute(col_function_cell, "text", 0)
 
 # COLUMN bool -- Plot the function?
-col_draw = gtk.TreeViewColumn("Plot")
+col_draw = gtk.TreeViewColumn()
 col_draw_cell = gtk.CellRendererToggle()
 col_draw_cell.set_property("activatable", True)
-col_draw_cell.connect("toggled", onToggle, (store_plot, 1))
+col_draw_cell.connect("toggled", onToggle, store_plot)
 col_draw.pack_start(col_draw_cell)
 col_draw.add_attribute(col_draw_cell, "active", 1)
 
@@ -60,3 +70,19 @@ tree_plot.append_column(col_function)
 draw_graph = ui_tree.get_widget("draw_graph")
 
 window_main = ui_tree.get_widget("window_main")
+
+# Prepare graph resolution
+xmin = ui_tree.get_widget("spinner_graphwindow_x_min")
+xmax = ui_tree.get_widget("spinner_graphwindow_x_max")
+ymin = ui_tree.get_widget("spinner_graphwindow_y_min")
+ymax = ui_tree.get_widget("spinner_graphwindow_y_max")
+
+def resolution():
+    return (xmin.get_value(), xmax.get_value(),
+        ymin.get_value(), ymax.get_value())
+
+# Set default windowing values
+xmin.set_value(-10.0)
+xmax.set_value(10.0)
+ymin.set_value(-10.0)
+ymax.set_value(10.0)
