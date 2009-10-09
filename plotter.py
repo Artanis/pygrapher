4c6ed3f40dddc015f7bca9956b2f88015c6bedba
Erik Youngren <artanis.00@gmail.com>
2009-10-08 23:18:27 -0700

    A class representation of a Function, with the color-gen thing, the ability to plot the function against a domain, and the ability to evaluate the function for a specific value

--- plotter.py
+++ plotter.py
@@ -1,6 +1,9 @@
+# -*- coding: UTF-8 -*-
+
+import hashlib
 from math import *
 
-__all__ = ['safe_dict']
+__all__ = ['Function']
 
 # I knew this app was going to borrow  heavily from Lybniz, but this
 # is going to get ridiculous... I'm going to have to figure everything
@@ -48,13 +51,13 @@ def marks(min_val,max_val,minor=1):
 	a = 0.2 # tweakable control for when to switch scales
 	          # big a value results in more marks
 
-	a = a + math.log10(minor)
+	a = a + log10(minor)
 
 	width = max_val - min_val
-	log10_range = math.log10(width)
+	log10_range = log10(width)
 
-	interval = 10 ** int(math.floor(log10_range - a))
-	lower_mark = min_val - math.fmod(min_val,interval)
+	interval = 10 ** int(floor(log10_range - a))
+	lower_mark = min_val - fmod(min_val,interval)
 	
 	if lower_mark < min_val:
 		lower_mark += interval
@@ -68,29 +71,80 @@ def marks(min_val,max_val,minor=1):
 
 # End Lybniz
 
-def precompile_plot(model, path, row_iter, plots):
-    if model[path][1]: function = model[path][0].replace("^","**")
-    else: function = ""
+class Function(object):
+    """ Defines a function of x and provides methods to evaluate that
+    function at any value of x, and to plot it against a domain.
     
-    if function != "":
+    """
+    
+    def __init__(self, function_definition):
+        self.__function = function_definition
+    
+    @property
+    def color():
+        """ A tuple containing the color of this function in RGBA space,
+        where the individual components are in the set of Rational
+        numbers between 0.0 and 1.0 inclusive.
+        
+        Generated automatically from the text of the function via MD5
+        hash, with each component taking 8 digits of the hexdigest.
+        
+        """
+        
+        color = hashlib.md5(self.__function).hexdigest()
+        return (
+            int(color[0:8],   16) / float(16**8),
+            int(color[8:16],  16) / float(16**8),
+            int(color[16:24], 16) / float(16**8),
+            int(color[24:32], 16) / float(16**8))
+    
+    def __compile(self):
+        """ Compiles the function text """
         try:
-            compiled = compile(function,"",'eval')
-            c = hashlib.md5(function).hexdigest()
-            r = float(int(c[0:10],  16)) / 16**10
-            g = float(int(c[10:20], 16)) / 16**10
-            b = float(int(c[20:30], 16)) / 16**10
-            plots.append((compiled, (r, g, b)))
-            model[path][2] = None
-        except SyntaxError:
-            model[path][2] = gtk.icon_theme_get_default().load_icon(
-                "gtk-dialog-warning", gtk.ICON_SIZE_MENU, 0)
-
-def plot(fn, x):
-    """ Generator """
+            return compile(self.__function, "", "eval")
+        except SyntaxError: 
+            print "Syntax Error:", self.function
+        except TypeError:
+            print "Type Error: null bytes in source", 
     
-    pass
-
-def trace():
-    pass
+    def plot(self, domain):
+        """ Generate coordinates for each real number provided in domain
+        
+        Named Arguments:
+        * domain -- a (finite) list of real numbers. Sorry, but
+          calculating values for domains of infinite size is just too
+          much work.
+        
+        """
+        function = self.__compile()
+        
+        namespace = safe_dict.copy()
+        
+        for x in domain:
+            yield self.evaluate(x, function, namespace)
+    
+    def evaluate(self, x, function=None, namespace=None):
+        """ Return coordinate for the given x value
+        
+        Named Arguments:
+        * x -- value to evaluate the function at
+        * function -- pre-compiled function ready for evaluation. Leave
+          default to have this function compile the function.
+        * namespace -- pre-existing namespace definition for safe
+          evaluation. Leave default to have this function create a safe
+          namespace from the safe_dict module variable.
+        
+        """
+        
+        function = self.__compile() if function is None else function
+        
+        namespace = safe_dict.copy() if namespace is None else namespace
+        namespace['x'] = x
+        
+        y = None
+        try: y = eval(function, {'__builtins__':{}}, namespace)
+        except: pass
+        
+        return (x, y)
 
 
