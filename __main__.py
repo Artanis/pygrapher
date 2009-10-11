0bf30a67fee964b6cc034c22a3629e851f05e274
Erik Youngren <artanis.00@gmail.com>
2009-10-11 01:44:06 -0700

    Moved all rendering code out of the expose event and into a more usable location. Some minor changes to the coord-space converters (comments, type conversions that always get used with them, etc.) Fixed the property definitions to actually work.

--- __main__.py
+++ __main__.py
@@ -4,93 +4,19 @@ import gtk
 import math
 from math import *
 from time import time
-import hashlib
 
 import ui
 import plotter
 import renderer
 
 def expose_graph (draw, event):
-    # Profiling
-    draw_start = time()
-    
     x, y, w, h = draw.get_allocation()
     cr = draw.window.cairo_create()
     
-    # Prepare Canvas
-    cr.set_source_rgb(1.0,1.0,1.0)
-    cr.rectangle(0,0,w,h)
-    cr.fill()    
+    # create renderer
+    canvas = renderer.Graph((w, h), ui.resolution())
     
-    # Prepare canvas <=> graph translations
-    xmin, xmax, ymin, ymax = ui.resolution()
-    canvas_x = lambda x: (x - xmin) * w / (xmax - xmin)
-    canvas_y = lambda y: (ymax - y) * h / (ymax - ymin)
-    canvas_point = lambda x,y: (canvas_x(x), canvas_y(y))
-    graph_x = lambda x: x * (xmax - xmin) / w + xmin
-    graph_y = lambda y: ymax - (y * (ymax - ymin) / h)
-    
-    origin_x = int(round(canvas_x(0)))
-    origin_y = int(round(canvas_y(0)))
-    
-    # Draw Coordinate Plane
-    # Draw cross
-    # Y axis
-    cr.set_source_rgb(0,0,0)
-    cr.move_to(origin_x, 0)
-    cr.line_to(origin_x, h)
-    cr.set_line_width(1.5)
-    cr.stroke()
-    # X axis
-    cr.set_source_rgb(0,0,0)
-    cr.move_to(0, origin_y)
-    cr.line_to(w, origin_y)
-    cr.set_line_width(1.5)
-    cr.stroke()
-    
-    for i in plotter.marks(xmin, xmax):
-        label = '%g' % i if i != 0 else ""
-        
-        cr.set_source_rgb(0,0,0)
-        cr.move_to(int(round(canvas_x(i))), origin_y - 5)
-        cr.line_to(int(round(canvas_x(i))), origin_y + 5)
-        cr.set_line_width(1)
-        cr.stroke()
-        
-        cr.set_source_rgb(0,0,0)
-        cr.move_to(int(round(canvas_x(i))) + -5 , origin_y + 15)
-        cr.text_path(label)
-        cr.fill()
-    
-    for i in plotter.marks(ymin, ymax):
-        label = '%g' % i if i != 0 else ""
-        
-        cr.set_source_rgb(0,0,0)
-        cr.move_to(origin_x - 5, int(round(canvas_y(i))))
-        cr.line_to(origin_x + 5, int(round(canvas_y(i))))
-        cr.set_line_width(1)
-        cr.stroke()
-        
-        cr.set_source_rgb(0,0,0)
-        cr.move_to(origin_x - 15, int(round(canvas_y(i))) + 5)
-        cr.text_path(label)
-        cr.fill()
-    
-    for i in plotter.marks(xmin, xmax, minor=10):
-        cr.set_source_rgb(0,0,0)
-        cr.move_to(int(round(canvas_x(i))), origin_y - 2)
-        cr.line_to(int(round(canvas_x(i))), origin_y + 2)
-        cr.set_line_width(1)
-        cr.stroke()
-    
-    for i in plotter.marks(ymin, ymax, minor=10):
-        cr.set_source_rgb(0,0,0)
-        cr.move_to(origin_x - 2, int(round(canvas_y(i))))
-        cr.line_to(origin_x + 2, int(round(canvas_y(i))))
-        cr.set_line_width(1)
-        cr.stroke()
-    
-    # Plot graphs
+    # Get graphs
     functions = []
     
     # kinda nice, this function definition anywhere ability.
@@ -104,75 +30,10 @@ def expose_graph (draw, event):
     
     del(foreach_cb) # Don't need it anymore.
     
-    if len(functions) > 0:
-        domain = map(graph_x, xrange(0, w, 1))
-        
-        for function in functions:
-            prev_drawn = False
-            for point in function.plot(domain):
-                p_x, p_y = point
-                
-                p_x = int(round(canvas_x(p_x)))
-                
-                p_y = int(round(canvas_y(p_y))) if p_y is not None else None
-                
-                if p_y is None or p_y < 0 or p_y > h:
-                    prev_drawn = False
-                else:
-                    if prev_drawn:
-                        cr.line_to(p_x, p_y)
-                        prev_drawn = True
-                    else:
-                        cr.move_to(p_x, p_y)
-                        prev_drawn = True
-            
-            r, g, b, a = function.color
-            cr.set_source_rgb(r, g, b)
-            cr.set_line_width(3 if function.highlight else 1)
-            cr.stroke()
+    graph = canvas.render(functions, ui.trace())
     
-    # Draw the trace line and highlight intersections (with coords)
-    b_trace, x_value = ui.trace()
-    if b_trace:
-        c_x = int(round(canvas_x(x_value)))
-        
-        cr.move_to(c_x, 0)
-        cr.line_to(c_x, h)
-        
-        cr.set_source_rgb(1,0,0)
-        cr.set_line_width(1)
-        cr.stroke()
-        
-        for function in functions:
-            x_value, y_value = function.evaluate(x_value)
-            
-            c_y = -1
-            if y_value is not None:
-                c_y = int(round(canvas_y(y_value)))
-            
-            if c_y > 0 and c_y < h:
-                cr.set_source_rgb(1,0,0)
-                cr.set_line_width(1)
-                cr.move_to(c_x - 5, c_y - 5)
-                cr.line_to(c_x + 5, c_y + 5)
-                cr.move_to(c_x + 5, c_y - 5)
-                cr.line_to(c_x - 5, c_y + 5)
-                cr.stroke()
-                
-                label = "(%0.4f, %0.4f)" % (x_value, y_value)
-                cr.move_to(c_x+10, c_y+10)
-                cr.text_path(label)
-                cr.set_source_rgba(1,1,1,0.75)
-                cr.set_line_width(6)
-                cr.stroke_preserve()
-                cr.set_source_rgb(0,0,0)
-                cr.fill()
-    
-    draw_time = time() - draw_start
-    cr.set_source_rgba(1,0,0,1)
-    cr.move_to(10,h-10)
-    cr.text_path(
-        "(%d x %d; %4f) %fms" % (w, h, w/float(h), draw_time * 1000))
+    cr.set_source_surface(graph)
+    cr.rectangle(0,0,w,h)
     cr.fill()
 
 def refresh_graph(btn):
--- renderer.py
+++ renderer.py
@@ -1,16 +1,22 @@
+import time
+import cairo
+
 import plotter
 
-class Canvas(object):
-    """ A paintboard that will get transfered to a destination surface
-    when the drawing is complete """
+def Property(fn):
+    return property(**fn())
+
+class Graph(object):
+    """ A paintboard for graphing that will get transfered to a
+    destination surface when the drawing is complete """
     
     def __init__(self, size, resolution):
         """ Initialize the canvas instance
         
         Named Arguments:
-        * size -- An iterable containing the width and height of the
+        * size -- A list containing the width and height of the
           target surface. Format: (width, height)
-        * resolution -- An interable containing the window of the graph
+        * resolution -- A list containing the window of the graph
           that will be drawn. Format: (x_min, x_max, y_min, y_max)
         
         """
@@ -18,7 +24,139 @@ class Canvas(object):
         self.size = list(size)
         self.resolution = list(resolution)
     
-    @property
+    def render(self, functions, trace=None):
+        origin_x, origin_y = self.canvas_point(0,0)
+        
+        # Set-up canvas and context
+        canvas = cairo.ImageSurface(cairo.FORMAT_ARGB32,
+            self.width, self.height)
+        cr = cairo.Context(canvas)
+        
+        # Profiling
+        total = time.time()
+        
+        # Blank canvas
+        cr.set_source_rgb(1.0,1.0,1.0)
+        cr.rectangle(0, 0, self.width, self.height)
+        cr.fill()
+        
+        # Drawing Coordinate Plane
+        cr.set_source_rgb(0,0,0)
+        cr.set_line_width(1)
+        
+        # X axis
+        cr.move_to(0, origin_y)
+        cr.line_to(self.width, origin_y)
+        
+        #Y axis
+        cr.move_to(origin_x, 0)
+        cr.line_to(origin_x, self.height)
+        
+        # Major ticks
+        for i in plotter.marks(self.xmin, self.xmax):
+            if i != 0:
+                cr.move_to(self.canvas_x(i), origin_y - 5)
+                cr.line_to(self.canvas_x(i), origin_y + 5)
+                
+                cr.move_to(self.canvas_x(i), origin_y)
+                cr.show_text(str(i))
+        
+        for i in plotter.marks(self.ymin, self.ymax):
+            if i != 0:
+                cr.move_to(origin_x - 5, self.canvas_y(i))
+                cr.line_to(origin_x + 5, self.canvas_y(i))
+                
+                cr.move_to(origin_x, self.canvas_y(i))
+                cr.show_text(str(i))
+        
+        # Minor ticks
+        for i in plotter.marks(self.xmin, self.xmax, 10):
+            if i != 0:
+                cr.move_to(self.canvas_x(i), origin_y - 2)
+                cr.line_to(self.canvas_x(i), origin_y + 2)
+        
+        for i in plotter.marks(self.xmin, self.xmax, 10):
+            if i != 0:
+                cr.move_to(origin_x - 2, self.canvas_y(i))
+                cr.line_to(origin_x + 2, self.canvas_y(i))
+        
+        cr.stroke()
+        
+        plot_start = time.time()
+        
+        # Function Plotting
+        if len(functions) > 0:
+            for function in functions:
+                prev_drawn = False
+                
+                for point in function.plot(self.domain()):
+                    x, y = point
+                    
+                    canvas_x = self.canvas_x(x)
+                    canvas_y = self.canvas_y(y) if y is not None else None
+                    
+                    if canvas_y is not None and \
+                        canvas_y > 0 and canvas_y < self.height:
+                        
+                        if prev_drawn:
+                            cr.line_to(canvas_x, canvas_y)
+                        else:
+                            cr.move_to(canvas_x, canvas_y)
+                            prev_drawn = True
+                        
+                    else: prev_drawn = False
+                
+                r, g, b, a = function.color
+                cr.set_source_rgb(r, g, b)
+                cr.set_line_width(3 if function.highlight else 1)
+                cr.stroke()
+        
+        print "functions:", (time.time() - plot_start) * 1000
+        
+        trace_start = time.time()
+        
+        if trace is not None:
+            canvas_x = self.canvas_x(trace)
+            
+            cr.set_source_rgb(1, 0, 0)
+            cr.set_line_width(1)
+            cr.move_to(canvas_x, 0)
+            cr.line_to(canvas_x, self.height)
+            
+            for function in functions:
+                x, y = function.evaluate(trace)
+                
+                canvas_y = self.canvas_y(y) if y is not None else -1
+                
+                if canvas_y > 0 and canvas_y < self.height:
+                    cr.move_to(canvas_x - 5, canvas_y - 5)
+                    cr.line_to(canvas_x + 5, canvas_y + 5)
+                    cr.move_to(canvas_x + 5, canvas_y - 5)
+                    cr.line_to(canvas_x - 5, canvas_y + 5)
+            cr.stroke()
+        
+        print "Trace:", (time.time() - trace_start) * 1000
+        
+        print "Total:", (time.time() - total) * 1000
+        
+        return canvas
+    
+    def domain(self):
+        """ Generator for the domain allowed by the window
+        
+        Preserves the memory saving feature of xrange by avoiding
+        mapping it to Graph.graph_x (which creates a list,) and
+        maintaining the simplicity of already having graphing space
+        coords to work with.
+        
+        """
+        
+        domain = xrange(0, self.width, 1)
+        
+        for x in domain:
+            yield self.graph_x(x)
+    
+    @Property
     def width():
         def fget(self):
             return self.size[0]
@@ -28,7 +166,7 @@ class Canvas(object):
         
         return locals()
     
-    @property
+    @Property
     def height():
         def fget(self):
             return self.size[1]
@@ -38,7 +176,7 @@ class Canvas(object):
         
         return locals()
     
-    @property
+    @Property
     def xmin():
         def fget(self):
             return self.resolution[0]
@@ -48,7 +186,7 @@ class Canvas(object):
         
         return locals()
     
-    @property
+    @Property
     def xmax():
         def fget(self):
             return self.resolution[1]
@@ -58,7 +196,7 @@ class Canvas(object):
         
         return locals()
     
-    @property
+    @Property
     def ymin():
         def fget(self):
             return self.resolution[2]
@@ -68,7 +206,7 @@ class Canvas(object):
         
         return locals()
     
-    @property
+    @Property
     def ymax():
         def fget(self):
             return self.resolution[3]
@@ -81,62 +219,42 @@ class Canvas(object):
     def canvas_x(self, x):
         """ Converts an x value from canvas space to graph space
         
-        Named Arguments:
-        * x -- The x value to convert
-        
         """
         
-        return (x - self.xmin) * self.width / (self.xmax - self.xmin)
+        return int(round((x - self.xmin) * self.width / (self.xmax - self.xmin)))
     
     def canvas_y(self, y):
         """ Converts a y value from canvas space to graph space
-                
-        Named Arguments:
-        * y -- The y value to convert
         
         """
         
-        return (self.ymax - y) * self.height / (self.ymax - self.ymin)
+        return int(round((self.ymax - y) * self.height / (self.ymax - self.ymin)))
     
-    def canvas_point(self, point):
+    def canvas_point(self, x, y):
         """ Converts a point from canvas space to graph space
         
-        Named Arguments:
-        * point -- A tuple containing x and y values in canvas space
-        
         """
         
-        x, y = point
         return (self.canvas_x(x), self.canvas_y(y))
     
     def graph_x(self, x):
         """ Converts an x value from graph space to canvas space
-                
-        Named Arguments:
-        * x -- The x value to convert
         
         """
         
-        return x * (self.xmax - self.xmin) / self.width + self.xmin
+        return x * (self.xmax - self.xmin) / float(self.width) + self.xmin
     
     def graph_y(self, y):
         """ Converts a y value from graph space to canvas space
-                
-        Named Arguments:
-        * y -- The y value to convert
         
         """
         
-        return self.ymax - (y * (self.ymax - self.ymin) / self.height)
+        return self.ymax - (y * (self.ymax - self.ymin) / float(self.height))
     
-    def graph_point(self, point):
+    def graph_point(self, x, y):
         """ Converts point from graph space to canvas space
-                
-        Named Arguments:
-        * point -- A tuple containing x and y values in graph space
         
         """
         
-        x, y = point
         return (self.graph_x(x), self.graph_y(y))
 
